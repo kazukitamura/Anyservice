a09996e0038deec0268f4a08c7430782547d2c87
田村 <kazukitamura0905@gmail.com>
2019-08-11 02:53:43 +0000

    6 commit

--- resources/views/admin/user/explanation.blade.php
+++ resources/views/admin/user/explanation.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>MyNews</title>
+        <title>Anyservice</title>
+        <link rel="stylesheet" href="../../css/explanation.css">
     </head>
     <body>
         <h1>初めての方へ</h1>
