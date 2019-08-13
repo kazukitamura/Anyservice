f1f71bb5a8fb8c739d392190261ee17157a85e3e
田村 <kazukitamura0905@gmail.com>
2019-08-13 05:01:23 +0000

    9commit 親ビューの作成

--- resources/views/admin/user/explanation.blade.php
+++ resources/views/admin/user/explanation.blade.php
@@ -6,17 +6,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Anyservice</title>
         <link rel="stylesheet" href="../../css/explanation.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Bootstrap本体 -->
+        <script src="../../js/app.js" defer></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script><!-- Scripts（Jquery） -->
+  　　  　<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script><!-- Scripts（bootstrapのjavascript） -->
     </head>
     <body>
         <h1>初めての方へ</h1>
         <img src="../../image/image01.JPG" alt="画像01">
         <p1>カテゴリ（どこをキレイにするか）とお住いを選択し探しましょう。</p1><br>
         <p1>作業希望日が決まっている場合は、検索条件を指定して絞り込むのがオススメです。</p1><br>
-        <p1>カテゴリ（どこをキレイにするか）とお住いを選択し探しましょう。</p1><br>
+        <p1　class="text-success">カテゴリ（どこをキレイにするか）とお住いを選択し探しましょう。</p1><br>
         <p1>依頼したいプロ（お店）が決まったら予約内容と希望日時を選択します。</p1><br>
+        <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
         <p1>「ご予約のステップに進む」からお客様情報を入力し確認画面へ進みます。。</p1><br>
         <p1>仮予約後、プロ（お店）からマイページ上のメッセージ機能を通して連絡があります。</p1><br>
         <p1>プロ（お店）とはこのようなやりとりを行います。</p1><br>
+        <p class="text-danger">.text-danger</p>
         <p1>カテゴリ（どこをキレイにするか）とお住いを選択し探しましょう。</p1><br>
     </body>
 </html>
