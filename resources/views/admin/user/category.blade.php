9621868ea2b11455c2ddb80bab6cf485fc29011b
田村 <kazukitamura0905@gmail.com>
2019-08-23 12:08:03 +0000

    viewのっ実装

--- resources/views/admin/user/category.blade.php
+++ resources/views/admin/user/category.blade.php
@@ -1,22 +1,32 @@
 @extends('layouts.master')
-@section('title', 'お問合せ')
+@section('title', 'カテゴリー一覧')
 @section('stylesheet')
  <link rel="stylesheet" href="../../css/category.css">
 @endsection
 
 @section('content')
   
- <h1>カテゴリー一覧</h1>
- <img src="../../image/image01.JPG" alt="画像01">
- <p1>カテゴリ（どこをキレイにするか）とお住いを選択し探しましょう。</p1><br>
- <p1>作業希望日が決まっている場合は、検索条件を指定して絞り込むのがオススメです。</p1><br>
- <p1　class="text-success">カテゴリ（どこをキレイにするか）とお住いを選択し探しましょう。</p1><br>
- <p1>依頼したいプロ（お店）が決まったら予約内容と希望日時を選択します。</p1><br>
- <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
- <p1>「ご予約のステップに進む」からお客様情報を入力し確認画面へ進みます。。</p1><br>
- <p1>仮予約後、プロ（お店）からマイページ上のメッセージ機能を通して連絡があります。</p1><br>
- <p1>プロ（お店）とはこのようなやりとりを行います。</p1><br>
- <p class="text-danger">.text-danger</p>
- <p1>カテゴリ（どこをキレイにするか）とお住いを選択し探しましょう。</p1><br>
+<header class="header">
+       <ul class="example">
+          <li><a href="{{ action('Admin\UserController@explanation')}}">あなたのマイスターとは?</a></li>
+          {{-- action 関数は、指定するコントローラアクションに対するURLを生成します。 --}}
+          <li>出店希望の方へ</li>
+          <li>お気に入り</li>
+       </ul>
+</header>
+    <div class="glovalNavigation">
+       <p>グローバルナビゲーション</p>
+    </div>
+    <div class="content">
+        <p>コンテンツ</p>
+        
+    </div>
+    <div class="localNavigation">
+        <p>ローカルナビゲーション</p>
+    </div>
+<footer class="footer">
+        <p>フッター</p>
+</footer>
+
 @endsection
 
